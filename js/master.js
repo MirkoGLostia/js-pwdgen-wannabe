d4aae5a709a5c61f2f07172816b240729d6cf486
MirkoGLostia <giggi.torre@mail.com>
2023-04-26 13:44:13 +0200

    js_getting-the-data

--- js/master.js
+++ js/master.js
@@ -7,21 +7,27 @@
 
 
 // variables
-
-
+let utName, utSurname, favColor;
 
 
 // chiedere nome utente
-
+utName = prompt('come ti chiami?');
+console.log(utName);
 
 
 // chiedere cognome utente 
+utSurname = prompt('e il tuo cognome?');
+console.log(utSurname);
 
 
 // chiedere colore preferito
+favColor = prompt('quale colore preferisci?');
+console.log(favColor);
 
 
 // generare password con i dati
+const password = (utName + utSurname + favColor + '21');
+console.log(password);
 
 
 // mostrare il risultato in pagina
