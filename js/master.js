25bc8c8c3dbabc570a21532641a1a7fc31762894
MirkoGLostia <giggi.torre@mail.com>
2023-04-26 17:39:16 +0200

    css_bonus

--- js/master.js
+++ js/master.js
@@ -12,23 +12,28 @@ let utName, utSurname, favColor;
 
 // chiedere nome utente
 utName = prompt('come ti chiami?');
-console.log(utName);
 
 
 // chiedere cognome utente 
 utSurname = prompt('e il tuo cognome?');
-console.log(utSurname);
 
 
 // chiedere colore preferito
 favColor = prompt('quale colore preferisci?');
-console.log(favColor);
 
 
 // generare password con i dati
 const password = (utName + utSurname + favColor + '21');
-console.log(password);
 
 
 // mostrare il risultato in pagina
 document.getElementById('generated_psw').innerHTML = password
+
+
+// BONUS
+
+document.getElementById('user_name').innerHTML = utName
+
+document.getElementById('user_surname').innerHTML = utSurname
+
+document.getElementById('user_color').innerHTML = favColor
