26218118e9e45a322686e6bf770a6f586304398b
MirkoGLostia <giggi.torre@mail.com>
2023-04-26 13:22:35 +0200

    js_consegna-e-suddivisione-dei-compiti

---
+++ js/master.js
@@ -0,0 +1,27 @@
+// richiesta
+
+    // Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito.
+    // Infine scrivi sulla pagina: "nomecognomecolorepreferito21"
+
+
+
+
+// variables
+
+
+
+
+// chiedere nome utente
+
+
+
+// chiedere cognome utente 
+
+
+// chiedere colore preferito
+
+
+// generare password con i dati
+
+
+// mostrare il risultato in pagina
